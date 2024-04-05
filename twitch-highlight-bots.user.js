a9078faa74364c7b4e780b3467f7375fe6d88d1d
İsmail Karslı <17887754+ismailkarsli@users.noreply.github.com>
2024-04-05 23:32:52 +0300

    7TV compatibility

--- twitch-highlight-bots.user.js
+++ twitch-highlight-bots.user.js
@@ -7,7 +7,7 @@
 // @homepageURL https://github.com/ismailkarsli/userscripts
 // @supportURL https://github.com/ismailkarsli/userscripts/issues
 // @updateURL https://raw.githubusercontent.com/ismailkarsli/userscripts/main/twitch-highlight-bots.user.js
-// @version 1.0.0
+// @version 1.0.1
 // @match https://www.twitch.tv/*
 // @grant none
 // ==/UserScript==
@@ -18,8 +18,8 @@ const MutationObserver =
 const observer = new MutationObserver((mutations, observer) => {
     for (const mutation of mutations) {
         for (const addedNode of mutation.addedNodes) {
-            if (!addedNode?.classList?.contains("chat-line__message")) continue;
-            const username = addedNode.querySelector(".chat-author__display-name")?.innerText;
+            if (!addedNode?.classList?.contains("chat-line__message") && !addedNode?.classList?.contains("seventv-message")) continue;
+            const username = (addedNode.querySelector(".chat-author__display-name") || addedNode.querySelector(".seventv-chat-user-username"))?.innerText;
             if (!username) continue;
             const isBot = bots.has(username.toLowerCase());
             if (isBot) {
